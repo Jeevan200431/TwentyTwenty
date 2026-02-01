63dee74bf09e51aaf496d1bcca952316efcb585d
JeevanMR <jeevan200431@gmail.com>
2026-02-01 11:45:06 +0530

    Initial working login register portfolio app

---
+++ client/script.js
@@ -0,0 +1,49 @@
+const API = "http://localhost:5000/api";
+
+function register() {
+  fetch(`${API}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: regEmail.value,
+      password: regPassword.value
+    })
+  })
+  .then(res => res.json())
+  .then(data => message.innerText = data.message);
+}
+
+function login() {
+  fetch(`${API}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: loginEmail.value,
+      password: loginPassword.value
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("email", data.email);
+      window.location.href = "portfolio.html";
+    } else {
+      message.innerText = data.message;
+    }
+  });
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "index.html";
+}
+
+if (window.location.pathname.includes("portfolio")) {
+  if (!localStorage.getItem("token")) {
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("userEmail").innerText =
+      "Logged in as: " + localStorage.getItem("email");
+  }
+}
